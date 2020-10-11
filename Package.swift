dc20face7f56578ca6d7af03d0c2f2dbbc512f28
William Ughetta <wughetta@princeton.edu>
2020-10-11 18:53:52 -0400

    Original macocr code

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "macocr",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
     ],
--- Sources/macocr/main.swift
+++ Sources/macocr/main.swift
@@ -1,1 +1,32 @@
-print("Hello, world!")
+import Cocoa
+import Vision
+
+func main(args: [String]) -> Int32 {
+    guard CommandLine.arguments.count == 3 else {
+        fputs("Usage: vn [image] [dst]\n", stderr)
+        return 1
+    }
+
+    let (src, dst) = (args[1], args[2])
+
+    guard let img = NSImage(byReferencingFile: src) else {
+        fputs("Error: failed to load image '\(src)'\n", stderr)
+        return 1
+    }
+
+    guard let imgRef = img.cgImage(forProposedRect: &img.alignmentRect, context: nil, hints: nil) else {
+        fputs("Error: failed to convert NSImage to CGImage for '\(src)'\n", stderr)
+        return 1
+    }
+
+    let request = VNRecognizeTextRequest { (request, error) in
+        let observations = request.results as? [VNRecognizedTextObservation] ?? []
+        let obs : [String] = observations.map { $0.topCandidates(1).first?.string ?? ""}
+        try? obs.joined(separator: "\n").write(to: URL(fileURLWithPath: dst), atomically: true, encoding: String.Encoding.utf8)
+    }
+
+    try? VNImageRequestHandler(cgImage: imgRef, options: [:]).perform([request])
+
+    return 0
+}
+exit(main(args: CommandLine.arguments))
